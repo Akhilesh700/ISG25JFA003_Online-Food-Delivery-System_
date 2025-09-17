8c21926a99c953dc48c5cafce11ad2bb8f20ad13
Singh <2441326@cognizant.com>
2025-09-17 22:03:55 +0530

    Refactored and Added : Refactored Signup service to increase code reusability of user creation and added delivery agent signup service

--- src/main/java/com/cognizant/onlinefooddeliverysystem/security/SignupService.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/security/SignupService.java
@@ -2,34 +2,50 @@ package com.cognizant.onlinefooddeliverysystem.security;
 
 import com.cognizant.onlinefooddeliverysystem.dto.customer.CustomerSignUpRequestDto;
 import com.cognizant.onlinefooddeliverysystem.dto.customer.CustomerSignUpResponseDto;
+import com.cognizant.onlinefooddeliverysystem.dto.deliveryagent.DeliveryAgentSignUpRequestDto;
+import com.cognizant.onlinefooddeliverysystem.dto.deliveryagent.DeliveryAgentSignUpResponseDto;
 import com.cognizant.onlinefooddeliverysystem.dto.restaurant.RestaurantSignupRequestDto;
 import com.cognizant.onlinefooddeliverysystem.dto.restaurant.RestaurantSignupResponseDto;
 import com.cognizant.onlinefooddeliverysystem.exception.signup.UserAlreadyExistsException;
 import com.cognizant.onlinefooddeliverysystem.model.Customer;
+import com.cognizant.onlinefooddeliverysystem.model.DeliveryAgent;
 import com.cognizant.onlinefooddeliverysystem.model.Restaurant;
 import com.cognizant.onlinefooddeliverysystem.model.User;
 import com.cognizant.onlinefooddeliverysystem.repository.CustomerRepository;
+import com.cognizant.onlinefooddeliverysystem.repository.DeliveryAgentDao;
 import com.cognizant.onlinefooddeliverysystem.repository.RestaurantRepository;
 import com.cognizant.onlinefooddeliverysystem.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class SignupService {
 
     private final UserRepository userRepository;
     private final CustomerRepository customerRepository;
     private final RestaurantRepository restaurantRepository;
+    private final DeliveryAgentDao deliveryAgentDao;
 
-    public CustomerSignUpResponseDto customerSignUp(CustomerSignUpRequestDto customerSignUpRequestDto) {
-        User user = userRepository.findUserByEmail(customerSignUpRequestDto.getEmail()).orElse(null);
+    public User createUser(String email, String password, User.UserRole role){
+        User user = userRepository.findUserByEmail(email).orElse(null);
         if(user != null) throw new UserAlreadyExistsException("User already exists with user ID : "+ user.getUserId());
         user = userRepository.save(User.builder()
-                .email(customerSignUpRequestDto.getEmail())
-                .password(customerSignUpRequestDto.getPassword())
-                .role(User.UserRole.valueOf("ROLE_CUSTOMER"))
+                .email(email)
+                .password(password)
+                .role(role)
                 .build()
+        );
+        return user;
+    }
+
+    public CustomerSignUpResponseDto customerSignUp(CustomerSignUpRequestDto customerSignUpRequestDto) {
+        User user = createUser(
+                customerSignUpRequestDto.getEmail(),
+                customerSignUpRequestDto.getPassword(),
+                User.UserRole.valueOf("ROLE_CUSTOMER")
         );
         Customer customer = customerRepository.save(Customer.builder()
                 .dob(customerSignUpRequestDto.getDob())
@@ -50,13 +66,10 @@ public class SignupService {
     }
 
     public RestaurantSignupResponseDto restaurantSignUp(RestaurantSignupRequestDto restaurantSignupRequestDto) {
-        User user = userRepository.findUserByEmail(restaurantSignupRequestDto.getEmail()).orElse(null);
-        if(user != null) throw new UserAlreadyExistsException("User already exists with user ID : "+ user.getUserId());
-        user = userRepository.save(User.builder()
-                .email(restaurantSignupRequestDto.getEmail())
-                .password(restaurantSignupRequestDto.getPassword())
-                .role(User.UserRole.valueOf("ROLE_RESTAURANT"))
-                .build()
+        User user = createUser(
+                restaurantSignupRequestDto.getEmail(),
+                restaurantSignupRequestDto.getPassword(),
+                User.UserRole.valueOf("ROLE_RESTAURANT")
         );
         Restaurant restaurant = restaurantRepository.save(Restaurant.builder()
                 .name(restaurantSignupRequestDto.getName())
@@ -74,5 +87,28 @@ public class SignupService {
                 user.getUserId(),
                 restaurant.getRestId()
         );
+    }
+
+    public DeliveryAgentSignUpResponseDto deliveryAgentSignUp(DeliveryAgentSignUpRequestDto deliveryAgentSignUpRequestDto) {
+        User user = createUser(
+                deliveryAgentSignUpRequestDto.getEmail(),
+                deliveryAgentSignUpRequestDto.getPassword(),
+                User.UserRole.valueOf("ROLE_DELIVERY_AGENT")
+        );
+
+        DeliveryAgent agent = deliveryAgentDao.save(DeliveryAgent.builder()
+                        .name(deliveryAgentSignUpRequestDto.getName())
+                        .identityProofNo(deliveryAgentSignUpRequestDto.getIdentityProofNo())
+                        .identityProofType(deliveryAgentSignUpRequestDto.getIdentityProofType())
+                        .phone(deliveryAgentSignUpRequestDto.getPhone())
+                        .status(DeliveryAgent.Status.Offline)
+                        .user(user)
+                .build()
+        );
+        return new DeliveryAgentSignUpResponseDto(
+                agent.getAgentId(),
+                agent.getStatus()
+        );
+
     }
 }
