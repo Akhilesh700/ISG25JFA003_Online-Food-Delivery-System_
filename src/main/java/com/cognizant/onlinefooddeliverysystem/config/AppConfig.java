4fee67c2ab21664544b5faf92162c1f482fa5ca2
Singh <2441326@cognizant.com>
2025-09-18 18:21:56 +0530

    Added : Bean of ModelMapper is added in AppConfig

--- src/main/java/com/cognizant/onlinefooddeliverysystem/config/AppConfig.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/config/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class randomConfig {
+public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
