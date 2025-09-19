6001e54b9902f2a2412847c1a47eeb923153206f
Singh <2441326@cognizant.com>
2025-09-19 19:41:40 +0530

    Ignore :

---
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/model/OrderItem.java
@@ -0,0 +1,35 @@
+package com.cognizant.onlinefooddeliverysystem.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_item_id")
+    private long cartItemId;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price")
+    private float price;
+
+    //Relationship mapping
+    @JoinColumn(name = "menu_item_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    private MenuItems menuItems;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    @JsonBackReference// This will be the foreign key column in the CartItem table
+    private Cart cart;
+
+}
