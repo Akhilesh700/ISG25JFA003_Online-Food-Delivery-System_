9bbcb78d98b44f4a370d9644e8869771026473a8
Singh <2441326@cognizant.com>
2025-09-20 23:45:44 +0530

    Updated : Updated the return type of placeOrder controller method from Order to PlaceOrderResponseDTO

--- src/main/java/com/cognizant/onlinefooddeliverysystem/controller/OrderController.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/controller/OrderController.java
@@ -1,9 +1,7 @@
 package com.cognizant.onlinefooddeliverysystem.controller;
 import com.cognizant.onlinefooddeliverysystem.dto.order.PlaceOrderRequestDto;
-import com.cognizant.onlinefooddeliverysystem.model.Order;
+import com.cognizant.onlinefooddeliverysystem.dto.order.PlaceOrderResponseDto;
 import com.cognizant.onlinefooddeliverysystem.service.OrderService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +15,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/place/{cartItemId}")
-    public ResponseEntity<Order> placeOrder(@PathVariable Long cartItemId, @RequestBody PlaceOrderRequestDto request) {
-        Order newOrder = orderService.placeOrderByCartId(cartItemId, request);
-        return new ResponseEntity<>(newOrder, HttpStatus.CREATED);
+    public ResponseEntity<PlaceOrderResponseDto> placeOrder(@PathVariable Long cartItemId, @RequestBody PlaceOrderRequestDto request) {
+        return new ResponseEntity<>(orderService.placeOrderByCartId(cartItemId, request), HttpStatus.CREATED);
     }
 }
