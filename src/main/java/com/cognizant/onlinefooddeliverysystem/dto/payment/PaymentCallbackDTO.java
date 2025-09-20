8eb2b90cef2948b85176a7b582b236a74d793624
Sankalp <sankalpbhagwan.salve@cognizant.com>
2025-09-20 21:30:41 +0530

    Ignore:

--- src/main/java/com/cognizant/onlinefooddeliverysystem/dto/payment/PaymentCallbackDTO.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/dto/payment/PaymentCallbackDTO.java
@@ -1,8 +1,10 @@
 package com.cognizant.onlinefooddeliverysystem.dto.payment;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PaymentCallbackDTO {
     private Integer paymentId;
     private String type;
