6801b97c0e8d9381d9f4821ba019056db7c84a37
Singh <2441326@cognizant.com>
2025-09-23 10:25:01 +0530

    Added : Added DTO for response of order history

--- src/main/java/com/cognizant/onlinefooddeliverysystem/dto/order/GetOrderHistoryResponseDto.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/dto/order/GetOrderHistoryResponseDto.java
@@ -1,4 +1,19 @@
 package com.cognizant.onlinefooddeliverysystem.dto.order;
 
+import com.cognizant.onlinefooddeliverysystem.model.MenuItems;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class GetOrderHistoryResponseDto {
+    private int orderId;
+    private String restaurant;
+    private String address;
+    private float totalPrice;
+    private List<MenuItems> menuItems;
+    private String status;
+    private String specialRequest;
 }
