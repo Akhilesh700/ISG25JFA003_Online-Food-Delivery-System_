0567f9707cc4dc380e96c2acdab2c03bbb4dcf5b
Singh <2441326@cognizant.com>
2025-09-23 18:52:47 +0530

    Added : Added a component GetVerifiedUser for getting the verified user from SecurityContextHolder

--- src/main/java/com/cognizant/onlinefooddeliverysystem/service/GetVerifiedUser.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/service/GetVerifiedUser.java
@@ -1,9 +1,27 @@
 package com.cognizant.onlinefooddeliverysystem.service;
 
+import com.cognizant.onlinefooddeliverysystem.exception.ResourceNotFoundException;
 import com.cognizant.onlinefooddeliverysystem.model.User;
+import com.cognizant.onlinefooddeliverysystem.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
-public class GetVerifiedUserFromSecurityContextHolder {
+@RequiredArgsConstructor
+@Component
+public class GetVerifiedUser {
+    private final UserRepository userRepository;
+
     public User getVerifiedUser(){
-        
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null){
+            throw new ResourceNotFoundException("User is not found!");
+        }
+        String username = (String) authentication.getPrincipal();
+        return userRepository.findUserByEmail(username).orElseThrow(() ->
+                new ResourceNotFoundException("User not found with email : " + username)
+        );
+
     }
 }
