e5db525c7855b5604d762dea951cdb5e2d7c9ade
Singh <2441326@cognizant.com>
2025-09-23 10:23:17 +0530

    Feat : Added functionality of Role Based Authorisation by setting the role of user to Security Context Holder

--- src/main/java/com/cognizant/onlinefooddeliverysystem/security/JWTFilter.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/security/JWTFilter.java
@@ -7,16 +7,17 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -36,24 +37,35 @@ public class JWTFilter extends OncePerRequestFilter {
                 filterChain.doFilter(request, response);
                 return;
             }
-//      Removing the "Bearer " part from the token
+//         Removing the "Bearer " part from the token
             String token = requestTokenHeader.split("Bearer ")[1];
-//        Function to get username from the token
+//         Function to get username from the token
             String username = authUtil.getUsernameFromToken(token);
+//            Finding user with the email
+            User user = userRepository.findUserByEmail(username).orElseThrow(
+                    () -> new NoUsersFoundWithUsername("User not found with username : " + username)
+            );
 
+//           Getting "role" from the user
+            String role = user.getRole().toString();
 //        Checking if the username exists and Authentication of SecurityContextHolder is not set
-            if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
-    //           Fetching the user with the username from DAO
-                User user = userRepository.findUserByEmail(username).orElseThrow(
-                        () -> new NoUsersFoundWithUsername("User not found with username : " + username)
-                );
-    //            Creating the authentication token by user, credentials and user's authorities
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-    //            Finally setting the Authentication of SecurityContextHolder
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                if (role == null) {
+                    logger.warn("JWT for user" +  user.getUserId() + "has an invalid role claim.");
+                    filterChain.doFilter(request, response);
+                    return; // Stop processing and reject the token
+                }
+
+//               Creating a list of authorities from the role string
+                List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(role));
+
+//            Creating the authentication token by username, credentials and user's authorities
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
+//            Finally setting the Authentication of SecurityContextHolder
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-    //            Moving to next filter
 
             }
+//            Moving to next filter
         filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/cognizant/onlinefooddeliverysystem/security/WebSecurityConfig.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/security/WebSecurityConfig.java
@@ -1,20 +1,14 @@
 package com.cognizant.onlinefooddeliverysystem.security;
 
-import com.cognizant.onlinefooddeliverysystem.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,7 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableMethodSecurity // this will enable @PreAuthorize
+//@EnableMethodSecurity // this will enable @PreAuthorize
 public class WebSecurityConfig {
     @Value("${api.version.path}")
     private String apiVersionPath;
@@ -43,15 +37,15 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         http
-                .csrf(csrfConfig -> csrfConfig.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         // Permit all requests to your authentication endpoints
                         .requestMatchers(apiVersionPath + "/auth/**").permitAll()
 //                      // Require authentication for all other requests
-//                                .requestMatchers(apiVersionPath + "/customer/**").hasAuthority("ROLE_CUSTOMER")
-//                                .requestMatchers(apiVersionPath + "/restaurant/**").hasAuthority("ROLE_RESTAURANT")
-//                                .requestMatchers(apiVersionPath + "/admin/**").hasAuthority("ROLE_ADMIN")
-                                .anyRequest().authenticated()
+                        .requestMatchers(apiVersionPath + "/customer/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_CUSTOMER")
+                        .requestMatchers(apiVersionPath + "/restaurant/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_RESTAURANT")
+                        .requestMatchers(apiVersionPath + "/admin/**").hasAuthority("ROLE_ADMIN")
+                        .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(customAuthenticationEntryPoint))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
