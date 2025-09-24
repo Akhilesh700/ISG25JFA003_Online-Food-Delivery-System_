8d68f3e5c7cbcdd96ed2b343f046a0e95f918962
Singh <2441326@cognizant.com>
2025-09-24 17:34:11 +0530

    Feat : Added logic for updating lastLoginAt value of user after each login

--- src/main/java/com/cognizant/onlinefooddeliverysystem/security/LoginService.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/security/LoginService.java
@@ -3,11 +3,14 @@ package com.cognizant.onlinefooddeliverysystem.security;
 import com.cognizant.onlinefooddeliverysystem.dto.login.LoginRequestDto;
 import com.cognizant.onlinefooddeliverysystem.dto.login.LoginResponseDto;
 import com.cognizant.onlinefooddeliverysystem.model.User;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -15,17 +18,21 @@ public class LoginService {
     private final AuthUtil authUtil;
     private final AuthenticationManager authenticationManager;
 
+    @Transactional
     public LoginResponseDto login(LoginRequestDto loginRequestDto) {
 //        Authentication of User from DAO
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword())
-            );
-            User user = (User) authentication.getPrincipal();
+        Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword())
+        );
 
-//      Generating the auth token with user object
+        User user = (User) authentication.getPrincipal();
+//        Updating the lastLoginAt to current time
+        user.setLastLoginAt(LocalDateTime.now());
+
+//        Generating the auth token with user object
         String authToken = authUtil.generateJwtToken(user);
 
-//      Returning the loginResonseDto object with authToken and userId
+//        Returning the DTO object with authToken and userId
         return new LoginResponseDto(authToken, user.getUserId());
     }
 }
