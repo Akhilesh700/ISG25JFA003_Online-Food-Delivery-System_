dd040563f601581d46e56276030a9dfec13b8fb0
Singh <2441326@cognizant.com>
2025-09-25 13:58:11 +0530

    Fix : Handled exception for expired JWT token

--- src/main/java/com/cognizant/onlinefooddeliverysystem/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/security/CustomAuthenticationEntryPoint.java
@@ -1,8 +1,6 @@
 package com.cognizant.onlinefooddeliverysystem.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
@@ -29,7 +27,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         Map<String, String> errorResponse = new HashMap<>();
         errorResponse.put("status", "401");
         errorResponse.put("error", "Unauthorized");
-        errorResponse.put("message", authException.getLocalizedMessage() + " So, you are not authorised to access this endpoint.");
+        errorResponse.put("message", "Invalid authorization token or Invalid access role");
 
         // Write the JSON response to the client
         response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
--- src/main/java/com/cognizant/onlinefooddeliverysystem/security/JWTFilter.java
+++ src/main/java/com/cognizant/onlinefooddeliverysystem/security/JWTFilter.java
@@ -4,12 +4,16 @@ import com.cognizant.onlinefooddeliverysystem.exception.login.NoUsersFoundWithUs
 import com.cognizant.onlinefooddeliverysystem.exception.order.UserHasNoRoleException;
 import com.cognizant.onlinefooddeliverysystem.model.User;
 import com.cognizant.onlinefooddeliverysystem.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -18,7 +22,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.security.SignatureException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -27,6 +34,7 @@ public class JWTFilter extends OncePerRequestFilter {
 
     private final AuthUtil authUtil;
     private final UserRepository userRepository;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -41,6 +49,7 @@ public class JWTFilter extends OncePerRequestFilter {
 //         Removing the "Bearer " part from the token
             String token = requestTokenHeader.split("Bearer ")[1];
 //         Function to get username from the token
+        try {
             String username = authUtil.getUsernameFromToken(token);
 //            Finding user with the email
             User user = userRepository.findUserByEmail(username).orElseThrow(
@@ -50,23 +59,35 @@ public class JWTFilter extends OncePerRequestFilter {
 //           Getting "role" from the user
             String role = user.getRole().toString();
 //        Checking if the username exists and Authentication of SecurityContextHolder is not set
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
-                if (role == null) {
-                    logger.warn("User " +  user.getUserId() + " has an invalid role");
-                    filterChain.doFilter(request, response);
-                    throw new UserHasNoRoleException("User " +  user.getUserId() + " has an invalid role");
+
+                if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                    if (role == null) {
+                        logger.warn("User " +  user.getUserId() + " has an invalid role");
+                        filterChain.doFilter(request, response);
+                        throw new UserHasNoRoleException("User " +  user.getUserId() + " has an invalid role");
+                    }
+
+    //               Creating a list of authorities from the role string
+                    List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(role));
+
+    //            Creating the authentication token by username, credentials and user's authorities
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
+    //            Finally setting the Authentication of SecurityContextHolder
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
                 }
+            } catch (ExpiredJwtException e) {
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.setContentType("application/json");
+                Map<String, String> errorResponse = new HashMap<>();
+                errorResponse.put("status", "401");
+                errorResponse.put("error", "Unauthorized");
+                errorResponse.put("message", "Authentication token expired. Please login again!");
 
-//               Creating a list of authorities from the role string
-                List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(role));
-
-//            Creating the authentication token by username, credentials and user's authorities
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
-//            Finally setting the Authentication of SecurityContextHolder
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-
+                response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+                return;
             }
-//            Moving to next filter
-        filterChain.doFilter(request, response);
+        //            Moving to next filter
+            filterChain.doFilter(request, response);
     }
 }
